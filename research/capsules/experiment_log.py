f05e6fa84bc83d7b983eccc1b6c86b7f23e140e5
Unknown <martvanblokland@hotmail.com>
2018-02-12 16:36:22 +0100

    fix

--- research/capsules/experiment_log.py
+++ research/capsules/experiment_log.py
@@ -182,8 +182,9 @@ def train_experiment(session, result, writer, last_step, max_steps, saver,
     step += 1
     summary, _ = session.run([result.summary, result.train_op])
     writer.add_summary(summary, i)
-    speed = (time.time() - start) / 128.
-    print("\rAverage speed per datapoint: {}".format(speed), end="")
+    if step % 100 == 0:
+        speed = (time.time() - start) / (128. * step)
+        print("\rAverage speed per datapoint: {}".format(speed), end="")
     if (i + 1) % save_step == 0:
       saver.save(
           session, os.path.join(summary_dir, 'model.ckpt'), global_step=i + 1)
